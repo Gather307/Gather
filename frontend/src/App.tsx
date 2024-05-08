785b23458b61759415efb658b89537b92d79f5b8
jhcp <jhagendo@calpoly.edu>
2024-05-07 21:06:57 -0700

    compact icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 import LoginPage from "./pages/LoginPage";
+import CompactItem from "./components/CompactItem";
 // import HomePage from './pages/HomePage';
 
 function App() {
@@ -12,6 +12,21 @@ function App() {
           <Route path="/" element={<LoginPage />} />{" "}
           {/* Hardcoded to test since don't have homepage yet*/}
           <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/ItemComp"
+            element={
+              <Box height="100vh" padding="50%">
+                <CompactItem
+                  name="TestItem"
+                  desc="Brown. My favorite. Other attributes."
+                  price={2.25}
+                  quant={1}
+                  pub={false}
+                  assigned={false}
+                />
+              </Box>
+            }
+          />
         </Routes>
       </Router>
     </ChakraProvider>
