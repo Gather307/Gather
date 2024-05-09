fc0d1f61d2d9a70334a3fcf9120003bf957b1a72
Jason <Jason1@jelincic.com>
2024-05-08 19:39:26 -0700

    feat: creates get all requests for groups and baskets

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,8 @@
+import mongoose from "mongoose";
 import { Express, Request, Response } from "express";
 import User, { IUser } from "./userSchema";
+import Group, { IGroup } from "./groupSchema";
+import Basket, { IBasket } from "./basketSchema";
 
 const express = require("express"); // 1. includes Express
 const app: Express = express(); // 2. initializes Express
@@ -12,6 +15,16 @@ app.get('/', (req, res) => {
 app.get("/users", async (req: Request, res: Response) => {
   const users = await User.find({});
   res.send(users);
+});
+
+app.get("/groups", async (req: Request, res: Response) => {
+  const groups = await Group.find({});
+  res.send(groups);
+});
+
+app.get("/baskets", async (req: Request, res: Response) => {
+  const baskets = await Basket.find({});
+  res.send(baskets);
 });
 
 app.listen(3001);
