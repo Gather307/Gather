3217c0f1b5c98c42ed79943c1277ea6db59740c8
Sofija Dimitrijevic <dimitrij@calpoly.edu>
2024-05-08 22:09:27 -0700

    finished signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 // import HomePage from './pages/HomePage';
 
 function App() {
@@ -9,9 +10,10 @@ function App() {
     <ChakraProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<LoginPage />} />{" "}
+          <Route path="/" element={<SignupPage />} />{" "}
           {/* Hardcoded to test since don't have homepage yet*/}
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </Router>
     </ChakraProvider>
