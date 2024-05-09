682ecd64b93dd79ebe62a9f409fa57035483d3fa
Jason <Jason1@jelincic.com>
2024-05-08 19:36:44 -0700

    feat: creates get all users api call

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,17 @@
-import { Express } from "express";
+import { Express, Request, Response } from "express";
+import User, { IUser } from "./userSchema";
+
 const express = require("express"); // 1. includes Express
 const app: Express = express(); // 2. initializes Express
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  const users = await User.find({});
+  res.send(users);
 });
 
 app.listen(3001);
