fa97d8f2a7da3d9bd6d602eb39b0980bd4ee1ba4
jhcp <jhagendo@calpoly.edu>
2024-05-11 23:18:53 -0700

    basic layout pretty much finished, colors missing

--- frontend/src/pages/GroupPage.tsx
+++ frontend/src/pages/GroupPage.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
 import { CompactGroupV1 } from "../components/CompactGroup";
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  HStack,
+  Heading,
+  Icon,
+  Text,
+} from "@chakra-ui/react";
 import SkeletonGroup from "../components/SkeletonGroup";
+import { IoIosSwap } from "react-icons/io";
+import SearchBar from "../components/SearchBar";
 
 export interface Group {
   name: string;
@@ -17,20 +28,40 @@ function GroupPage() {
 
   return (
     <>
-      <Box width="100%" bgColor="gray">
-        Header box.
+      <Box width="100%" height="5vw" alignContent="center" padding="0px 20px">
+        <HStack justifyContent="space-between" alignItems="center">
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            flexDir="row"
+          >
+            <Heading>Your GROUPS</Heading>
+            <Flex margin="10px 0px 0px 20px" alignItems="center">
+              <Icon as={IoIosSwap} marginRight="10px" />
+              <Text as="i">switch to items</Text>
+            </Flex>
+          </Box>
+          <Box>joingroupcomponent</Box>
+          <SearchBar
+            onSearch={() => console.log("Hello, reviewer.")}
+            placeholder="search for groups"
+            width="400px"
+          ></SearchBar>
+        </HStack>
       </Box>
       <Box
-        height="2px"
-        bgGradient="linear(to-r, rgba(0,0,0,0) 0%, rgba(1,1,1,1) 30%, rgba(0,0,0,0.6) 70%, rgba(0,0,0,0))"
+        height="4px"
+        bgGradient="linear(to-r, rgba(0,0,0,0) 10%, rgba(255,255,255,0.5) 30%, rgba(255,255,255,0.5) 70%, rgba(0,0,0,0) 90%)"
       />
       <Grid
         templateColumns="repeat(4, 20vw)"
         templateRows="repeat(2, 20vw)"
         gap="4vw"
         width="100vw"
-        height="100vh"
-        padding="4vw"
+        height="48vw"
+        padding="2vw"
+        justifyContent="center"
       >
         {loading
           ? skelIds.map((id) => (
