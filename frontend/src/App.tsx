6b35801ac487271a560855cd66301dd33099f9df
jhcp <jhagendo@calpoly.edu>
2024-05-16 23:00:39 -0700

    Fixed overflowing vertical display on all screens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
 import NavbarSignedOut from "./components/NavbarSignedOut";
 import NavbarSignedIn from "./components/NavbarSignedIn";
-import SearchBar from "./components/SearchBar";
+import GroupPage from "./pages/MyGroupsPage";
 
 function App() {
-  const userIsSignedIn = false; // was testing but placeholder for our authentication logic
+  const userIsSignedIn = true; // was testing but placeholder for our authentication logic
 
   return (
     <ChakraProvider>
       <Router>
-        {userIsSignedIn ? <NavbarSignedIn /> : <NavbarSignedOut />}
-        <Routes>
-          <Route path="/" element={<HomePage />} /> {/* this is a dummy page */}
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route
-            path="/search"
-            element={
-              <SearchBar
-                width="300px"
-                onSearch={() =>
-                  console.log(
-                    "Wow, you really just submitted information. How dare you."
-                  )
-                }
-                placeholder="DO NOT USE THIS."
-              />
-            }
-          />
-        </Routes>
+        <Box width="100vw" height="100vh" display="flex" flexDirection="column">
+          {userIsSignedIn ? <NavbarSignedIn /> : <NavbarSignedOut />}
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* this is a dummy page */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/groups" element={<GroupPage />} />
+          </Routes>
+        </Box>
       </Router>
     </ChakraProvider>
   );
