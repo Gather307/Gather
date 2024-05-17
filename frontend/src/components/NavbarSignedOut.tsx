1561397913c1919accd7ea164bb851780efdd221
jhcp <jhagendo@calpoly.edu>
2024-05-16 23:03:01 -0700

    Code formatting & UI formatting changes

--- frontend/src/components/NavbarSignedOut.tsx
+++ frontend/src/components/NavbarSignedOut.tsx
@@ -1,54 +1,52 @@
-import { Link } from 'react-router-dom';
-import {
-  Box,
-  Flex,
-  Text,
-  Button,
-  HStack,
-  Image,
-} from '@chakra-ui/react';
-import logo from '../../public/target.png';
+import { Link } from "react-router-dom";
+import { Box, Flex, Text, Button, HStack, Image } from "@chakra-ui/react";
+import logo from "../../public/target.png";
 
 const NavbarSignedOut = () => {
   return (
-    <Box bg={'#216869'} px={4}>
+    <Box bg={"#216869"} px={4} width="100vw">
       <Flex
-        minH={'60px'}
+        minH={"60px"}
         py={{ base: 2 }}
         px={{ base: 4 }}
-        align={'center'}
-        justify={'space-between'}>
+        align={"center"}
+        justify={"space-between"}
+      >
         <Flex align="center" ml={2}>
           <Link to="/">
             <Image src={logo} alt="Logo" boxSize="32px" mr={3} />
           </Link>
-          <Text fontSize="lg" color={'#DCE1DE'} ml={1}>Welcome, Gatherer!</Text>
+          <Text fontSize="lg" color={"#DCE1DE"} ml={1}>
+            Welcome, Gatherer!
+          </Text>
         </Flex>
-        <HStack spacing={8} alignItems={'center'}>
+        <HStack spacing={8} alignItems={"center"}>
           <Button
             as={Link}
             to="/login"
-            fontSize={'sm'}
+            fontSize={"sm"}
             fontWeight={400}
-            variant={'link'}
+            variant={"link"}
             _hover={{
-              textDecoration: 'underline',
-              color: '#DCE1DE'
+              textDecoration: "underline",
+              color: "#DCE1DE",
             }}
-            color={'#DCE1DE'}>
+            color={"#DCE1DE"}
+          >
             Sign In
           </Button>
           <Button
             as={Link}
             to="/signup"
-            fontSize={'sm'}
+            fontSize={"sm"}
             fontWeight={600}
-            color={'#DCE1DE'}
-            bg={'#49A078'}
+            color={"#DCE1DE"}
+            bg={"#49A078"}
             _hover={{
-              bg: '#9CC5A1',
-              color: '#1F2421'
-            }}>
+              bg: "#9CC5A1",
+              color: "#1F2421",
+            }}
+          >
             Sign Up
           </Button>
         </HStack>
