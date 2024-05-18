0c8d94da036de6797dae50e3dea60b9b542556a4
jhcp <jhagendo@calpoly.edu>
2024-05-17 20:56:07 -0700

    Added corners to skeleton group

--- frontend/src/components/SkeletonGroup.tsx
+++ frontend/src/components/SkeletonGroup.tsx
@@ -5,13 +5,19 @@ import {
   SkeletonText,
   VStack,
 } from "@chakra-ui/react";
+import "../styles/CompactGroup.css";
 
 interface Props {
   width: string;
   height: string;
+  corners?: boolean[];
 }
 
-const SkeletonGroup = ({ width, height }: Props) => {
+const SkeletonGroup = ({
+  width,
+  height,
+  corners = [false, false, false, false],
+}: Props) => {
   return (
     <Box
       width={width}
@@ -19,7 +25,36 @@ const SkeletonGroup = ({ width, height }: Props) => {
       backgroundColor="gray"
       padding="15px 40px 20px"
       borderRadius="25%"
+      position="relative"
     >
+      <Box
+        className="corner"
+        left="0px"
+        top="0px"
+        bgColor="gray"
+        display={corners[0] ? "inherit" : "none"}
+      />
+      <Box
+        className="corner"
+        right="0px"
+        top="0px"
+        bgColor="gray"
+        display={corners[1] ? "inherit" : "none"}
+      />
+      <Box
+        className="corner"
+        left="0px"
+        bottom="0px"
+        bgColor="gray"
+        display={corners[2] ? "inherit" : "none"}
+      />
+      <Box
+        className="corner"
+        right="0px"
+        bottom="0px"
+        bgColor="gray"
+        display={corners[3] ? "inherit" : "none"}
+      />
       <VStack height="100%" justifyContent="space-between">
         <Skeleton height="15%" width="50%" borderRadius="10px" />
         <SkeletonText
