bd7b811cf544fb5324a49c7efbc6dce363220828
jhcp <jhagendo@calpoly.edu>
2024-05-19 16:39:48 -0700

    Removed corners from skeletons

--- frontend/src/components/SkeletonGroup.tsx
+++ frontend/src/components/SkeletonGroup.tsx
@@ -10,51 +10,18 @@ import "../styles/CompactGroup.css";
 interface Props {
   width: string;
   height: string;
-  corners?: boolean[];
 }
 
-const SkeletonGroup = ({
-  width,
-  height,
-  corners = [false, false, false, false],
-}: Props) => {
+const SkeletonGroup = ({ width, height }: Props) => {
   return (
     <Box
       width={width}
       height={height}
-      backgroundColor="gray"
+      className="container"
       padding="15px 40px 20px"
       borderRadius="25%"
       position="relative"
     >
-      <Box
-        className="corner"
-        left="0px"
-        top="0px"
-        bgColor="gray"
-        display={corners[0] ? "inherit" : "none"}
-      />
-      <Box
-        className="corner"
-        right="0px"
-        top="0px"
-        bgColor="gray"
-        display={corners[1] ? "inherit" : "none"}
-      />
-      <Box
-        className="corner"
-        left="0px"
-        bottom="0px"
-        bgColor="gray"
-        display={corners[2] ? "inherit" : "none"}
-      />
-      <Box
-        className="corner"
-        right="0px"
-        bottom="0px"
-        bgColor="gray"
-        display={corners[3] ? "inherit" : "none"}
-      />
       <VStack height="100%" justifyContent="space-between">
         <Skeleton height="15%" width="50%" borderRadius="10px" />
         <SkeletonText
