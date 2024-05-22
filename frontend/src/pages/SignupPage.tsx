244cd2c13d6347a26ee8a1b21405fa573d93c735
Sofija Dimitrijevic <dimitrij@calpoly.edu>
2024-05-21 17:34:30 -0700

    signuppage finished

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Button,
   FormControl,
@@ -8,6 +9,7 @@ import {
   Link,
   Flex,
   Text,
+  HStack,
 } from "@chakra-ui/react";
 
 const SignupPage = () => {
@@ -15,40 +17,48 @@ const SignupPage = () => {
     <Flex
       align="center"
       justify="center"
-      height="100vh"
+      minHeight="100vh"
       width="100vw"
       bg="#DCE1DE"
+      p={4}
+      pt={{ base: "1rem", md: "0.5rem" }} // Reduced top padding to prevent overlap with navbar
     >
       <Box
         p={8}
-        width="full"
-        maxW="md"
+        width={{ base: "full", sm: "md", lg: "lg" }}
         borderWidth={1}
         borderRadius={8}
         boxShadow="lg"
         bg="white"
+        mt={{ base: "1rem", md: "0.5rem" }} // Reduced top margin to prevent overlap with navbar
       >
         <VStack spacing={6}>
           <Text fontSize="2xl" color="#216869">
             Create New Account
           </Text>
-          <FormControl id="firstName">
-            <FormLabel>First Name</FormLabel>
-            <Input
-              type="text"
-              borderColor="#216869"
-              _hover={{ borderColor: "#49A078" }}
-            />
-          </FormControl>
-          <FormControl id="lastName">
-            <FormLabel>Last Name</FormLabel>
-            <Input
-              type="text"
-              borderColor="#216869"
-              _hover={{ borderColor: "#49A078" }}
-            />
-          </FormControl>
-          <FormControl id="email">
+          <HStack
+            spacing={6}
+            width="full"
+            flexDirection={{ base: "column", md: "row" }}
+          >
+            <FormControl id="firstName" isRequired>
+              <FormLabel>First Name</FormLabel>
+              <Input
+                type="text"
+                borderColor="#216869"
+                _hover={{ borderColor: "#49A078" }}
+              />
+            </FormControl>
+            <FormControl id="lastName" isRequired>
+              <FormLabel>Last Name</FormLabel>
+              <Input
+                type="text"
+                borderColor="#216869"
+                _hover={{ borderColor: "#49A078" }}
+              />
+            </FormControl>
+          </HStack>
+          <FormControl id="email" isRequired>
             <FormLabel>Email</FormLabel>
             <Input
               type="email"
@@ -56,7 +66,15 @@ const SignupPage = () => {
               _hover={{ borderColor: "#49A078" }}
             />
           </FormControl>
-          <FormControl id="password">
+          <FormControl id="username" isRequired>
+            <FormLabel>Username</FormLabel>
+            <Input
+              type="text"
+              borderColor="#216869"
+              _hover={{ borderColor: "#49A078" }}
+            />
+          </FormControl>
+          <FormControl id="password" isRequired>
             <FormLabel>Password</FormLabel>
             <Input
               type="password"
@@ -64,7 +82,7 @@ const SignupPage = () => {
               _hover={{ borderColor: "#49A078" }}
             />
           </FormControl>
-          <FormControl id="confirmPassword">
+          <FormControl id="confirmPassword" isRequired>
             <FormLabel>Confirm Password</FormLabel>
             <Input
               type="password"
